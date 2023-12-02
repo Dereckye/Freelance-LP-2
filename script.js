1c4782aac47d03da9490efcfd7292fb3dc30e28c
Dereck Luiz dos Santos <107744801+Dereckye@users.noreply.github.com>
2023-12-02 02:13:47 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,66 @@
+"use strict";
+
+function updatePeopleCounter() {
+
+    const peopleCounterElement = document.getElementById("people-counter");
+  
+    let currentCount = parseInt(peopleCounterElement.innerText);
+  
+    const incrementValue = Math.random() > 0.5 ? 5 : 3;
+  
+    currentCount += incrementValue;
+  
+    peopleCounterElement.innerText = `${currentCount} Pessoas entraram no grupo`;
+  }
+
+  setInterval(updatePeopleCounter, 2000);
+
+
+// Select all slides
+const slides = document.querySelectorAll(".slide");
+
+// loop through slides and set each slides translateX
+slides.forEach((slide, indx) => {
+    slide.style.transform = `translateX(${indx * 100}%)`;
+});
+
+// select next slide button
+const nextSlide = document.querySelector(".btn-next");
+
+// current slide counter
+let curSlide = 0;
+// maximum number of slides
+let maxSlide = slides.length - 1;
+
+// add event listener and navigation functionality
+nextSlide.addEventListener("click", function () {
+    // check if current slide is the last and reset current slide
+    if (curSlide === maxSlide) {
+        curSlide = 0;
+    } else {
+        curSlide++;
+    }
+
+    //   move slide by -100%
+    slides.forEach((slide, indx) => {
+        slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+    });
+});
+
+// select next slide button
+const prevSlide = document.querySelector(".btn-prev");
+
+// add event listener and navigation functionality
+prevSlide.addEventListener("click", function () {
+    // check if current slide is the first and reset current slide to last
+    if (curSlide === 0) {
+        curSlide = maxSlide;
+    } else {
+        curSlide--;
+    }
+
+    //   move slide by 100%
+    slides.forEach((slide, indx) => {
+        slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+    });
+});
